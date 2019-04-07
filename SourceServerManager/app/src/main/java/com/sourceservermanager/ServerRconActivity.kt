1b8f28f923811393ea9c312b31f76106da646e5b
Lossy <thedragon1012@gmail.com>
2019-04-06 20:38:10 -0500

    Fancy ExtendedFloatingActionButton

--- SourceServerManager/app/src/main/java/com/sourceservermanager/ServerRconActivity.kt
+++ SourceServerManager/app/src/main/java/com/sourceservermanager/ServerRconActivity.kt
@@ -2,7 +2,6 @@ package com.sourceservermanager
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
 import android.os.AsyncTask
 import android.os.Bundle
 import android.os.Handler
